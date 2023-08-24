6c16d955915bbad62f17f5631c8cd747e2bc2e2b
bigyan-sapkota <bigyansapkotavines@gmail.com>
2023-08-24 11:58:16 +0545

    Completed final

--- 01_BRAND_PAGE/project-1/src/components/Hero.jsx
+++ 01_BRAND_PAGE/project-1/src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <main className="hero container">
       <div className="hero-content">
-        <h1>YOUR FEET DESERVETHE BEST</h1>
+        <h1>YOUR FEET DESERVE THE BEST</h1>
         <p>
           YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
           SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
