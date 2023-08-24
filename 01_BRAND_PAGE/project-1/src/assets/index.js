b767c48da58979ca1b3e3737406348e02080ed52
bigyan-sapkota <bigyansapkotavines@gmail.com>
2023-08-24 11:52:26 +0545

    completed

---
+++ 01_BRAND_PAGE/project-1/src/assets/index.js
@@ -0,0 +1,6 @@
+import Logo from "./brand_logo.png";
+import Amazon from "./amazon.png";
+import Flipkart from "./flipkart.png";
+import Shoe from "./shoe_image.png";
+
+export default { Logo, Amazon, Flipkart, Shoe };
---
+++ 01_BRAND_PAGE/project-1/src/components/Hero.jsx
@@ -0,0 +1,34 @@
+import Images from "../assets/index";
+
+const Hero = () => {
+  return (
+    <main className="hero container">
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVETHE BEST</h1>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.
+        </p>
+
+        <div className="hero-btn">
+          <button>Shop Now</button>
+          <button className="secondary-btn">Category</button>
+        </div>
+
+        <div className="shopping">
+          <p>Also Available On</p>
+          <div className="brand-icons">
+            <img src={Images.Amazon} alt="amazon_logo" />
+            <img src={Images.Flipkart} alt="flipkart_logo" />
+          </div>
+        </div>
+      </div>
+      <div className="hero-image">
+        <img src={Images.Shoe} alt="Shoe" />
+      </div>
+    </main>
+  );
+};
+
+export default Hero;
