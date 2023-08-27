b08a15a3c6c7abe357cc9fbf6a25ea9c49551bec
bigyan-sapkota <bigyansapkotavines@gmail.com>
2023-08-27 16:11:42 +0545

    Home Page completed

--- 03_DICE_GAME/project-3/src/components/StartGame.jsx
+++ 03_DICE_GAME/project-3/src/components/StartGame.jsx
@@ -5,8 +5,10 @@ import styled from "styled-components";
 const StartGame = () => {
   return (
     <Container>
-      <img src={Images.dices} alt="" />
       <div>
+        <img src={Images.dices} alt="" />
+      </div>
+      <div className="content">
         <h1>DICE GAME</h1>
         <Button>Play Now</Button>
       </div>
@@ -18,13 +20,38 @@ export default StartGame;
 
 const Container = styled.div`
   max-width: 1180px;
+  display: flex;
+  justify-content: flex-end;
+  height: 100vh;
+  margin: 0 auto;
+  align-items: center;
+
+  .content {
+    h1 {
+      font-size: 96px;
+      white-space: nowrap;
+    }
+  }
 `;
 
 const Button = styled.button`
+  float: right;
   color: #fff;
   padding: 10px 18px;
   background-color: #000;
   border: none;
   border-radius: 5px;
   min-width: 220px;
+  height: 44px;
+  font-size: 16px;
+  border: 1px solid transparent;
+  transition: 0.3s background ease-in;
+
+  &:hover {
+    background-color: #fff;
+    border: 1px solid black;
+    color: black;
+    cursor: pointer;
+    transition: 0.3s background ease-in;
+  }
 `;
