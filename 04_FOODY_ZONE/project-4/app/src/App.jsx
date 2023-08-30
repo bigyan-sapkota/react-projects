fcd9790ae46ba50a18afea3caf13fd65557fe680
bigyan-sapkota <bigyansapkotavines@gmail.com>
2023-08-30 22:40:20 +0545

    added search function

--- 04_FOODY_ZONE/project-4/app/src/App.jsx
+++ 04_FOODY_ZONE/project-4/app/src/App.jsx
@@ -7,6 +7,7 @@ export const BASE_URL = "http://localhost:9000";
 
 const App = () => {
   const [data, setData] = useState(null);
+  const [filteredData, setFilteredData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
@@ -17,6 +18,7 @@ const App = () => {
         const response = await fetch(BASE_URL);
         const json = await response.json();
         setData(json);
+        setFilteredData(json);
         setLoading(false);
       } catch (error) {
         setError("Unable to fetch data.");
@@ -26,32 +28,48 @@ const App = () => {
     fetchFoodData();
   }, []);
 
-  console.log(data);
+  const searchFood = (e) => {
+    const searchValue = e.target.value;
+
+    if (searchValue === "") {
+      setFilteredData(null);
+    }
+    const filter = data?.filter((food) =>
+      food.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+
+    setFilteredData(filter);
+  };
 
   if (error) return <div>{error}</div>;
   if (loading) return <div>Loading...</div>;
 
   return (
-    <div className="container">
-      <div className="topContainer">
-        <div className="logo">
-          <img src="/logo.svg" alt="logo" />
+    <>
+      <div className="container">
+        <div className="topContainer">
+          <div className="logo">
+            <img src="/logo.svg" alt="logo" />
+          </div>
+
+          <div className="search">
+            <input
+              onChange={searchFood}
+              type="text"
+              placeholder="Search Food..."
+            />
+          </div>
         </div>
 
-        <div className="search">
-          <input type="text" placeholder="Search Food..." />
+        <div className="filterContainer">
+          <Button text="All" />
+          <Button text="Breakfast" />
+          <Button text="Lunch" />
+          <Button text="Dinner" />
         </div>
       </div>
-
-      <div className="filterContainer">
-        <Button text="All" />
-        <Button text="Breakfast" />
-        <Button text="Lunch" />
-        <Button text="Dinner" />
-      </div>
-
-      <SearchResult data={data} />
-    </div>
+      <SearchResult data={filteredData} />
+    </>
   );
 };
 
